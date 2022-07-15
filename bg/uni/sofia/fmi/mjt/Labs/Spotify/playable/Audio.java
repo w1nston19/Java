a714510baa7bde95274969c66380e3b8ecdac045
w1nston19 <w1nston19@mail.bg>
2022-07-15 19:29:48 +0300

    Bug fixes (Playable constructor)

--- bg/uni/sofia/fmi/mjt/Labs/Spotify/playable/Audio.java
+++ bg/uni/sofia/fmi/mjt/Labs/Spotify/playable/Audio.java
@@ -2,7 +2,7 @@ package bg.uni.sofia.fmi.mjt.Labs.Spotify.playable;
 
 public class Audio extends PlayableAbstract implements Playable {
 
-    protected Audio(String title, String artist, int year, double duration) {
+    public Audio(String title, String artist, int year, double duration) {
         super(title, artist, year, duration);
     }
 
--- bg/uni/sofia/fmi/mjt/Labs/Spotify/playable/Video.java
+++ bg/uni/sofia/fmi/mjt/Labs/Spotify/playable/Video.java
@@ -1,7 +1,7 @@
 package bg.uni.sofia.fmi.mjt.Labs.Spotify.playable;
 
 public class Video extends PlayableAbstract implements Playable {
-    protected Video(String title, String artist, int year, double duration) {
+    public Video(String title, String artist, int year, double duration) {
         super(title, artist, year, duration);
     }
 
