7b625f6c2c9518e9ce22cee24249869f08e249b0
w1nston19 <w1nston19@mail.bg>
2022-07-15 14:49:06 +0300

    RentalService

---
+++ bg/uni/sofia/fmi/mjt/RentalService/Location/Location.java
@@ -0,0 +1,21 @@
+package bg.uni.sofia.fmi.mjt.RentalService.Location;
+
+public class Location {
+
+    private final double x;
+    private final double y;
+
+    public Location(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+}
---
+++ bg/uni/sofia/fmi/mjt/RentalService/Vehicle/Car.java
@@ -0,0 +1,15 @@
+package bg.uni.sofia.fmi.mjt.RentalService.Vehicle;
+
+public class Car extends VehicleAbstract implements Vehicle{
+
+    @Override
+    public double getPricePerMinute() {
+        return 0.50;
+    }
+
+    @Override
+    public String getType() {
+        return "CAR";
+    }
+
+}
